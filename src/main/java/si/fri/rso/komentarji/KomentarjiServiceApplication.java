eee15b5f30c8f50e4261dc7e940e7590af942e74
Jernej Vrhunc <jv4739@student.uni-lj.si>
2021-01-05 09:33:21 +0100

    Initial commit Komenatarji service

---
+++ src/main/java/si/fri/rso/komentarji/KomentarjiServiceApplication.java
@@ -0,0 +1,13 @@
+package si.fri.rso.komentarji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomentarjiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomentarjiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/si/fri/rso/komentarji/KomentarjiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package si.fri.rso.komentarji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomentarjiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
