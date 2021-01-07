994433eba152cb71914511170269e885e17e3aa3
Jernej Vrhunc <jv4739@student.uni-lj.si>
2021-01-07 22:49:34 +0100

    Init data configured.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO komentar (created, recept_id, komentar, ocena, uporabnik_id)
+        VALUES ('2016-2-1', 1, 'Nekaj najslabšega. Kdor se je spomnil tega lahko gre naravnost v zapor!', 1, 2);
+INSERT INTO komentar (created, recept_id, komentar, ocena, uporabnik_id)
+        VALUES ('2020-11-10', 1, 'Najboljsa sladica, kar sem jih poskusil narediti! Hvala vam!', 5, 3);
+INSERT INTO komentar (created, recept_id, komentar, ocena, uporabnik_id)
+        VALUES ('2017-3-12', 2, 'Gre lepo po grlu, vendar nekoliko preveč mesnato za moj okus.', 3, 3);
+INSERT INTO komentar (created, recept_id, komentar, ocena, uporabnik_id)
+        VALUES ('2019-11-12', 2, 'Preprosto slastno!', 5, 1);
+INSERT INTO komentar (created, recept_id, komentar, ocena, uporabnik_id)
+        VALUES ('2015-4-12', 3, 'Z družino smo jo jedli v hribih..nemorem se je najesti!', 5, 2);
+INSERT INTO komentar (created, recept_id, komentar, ocena, uporabnik_id)
+        VALUES ('2020-11-11', 3, 'Preveč zelja...', 2, 1);
+
+
